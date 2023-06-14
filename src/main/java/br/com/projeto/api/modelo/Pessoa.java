85a4e6a087870d42c725c30ebd0f375d7f5a203a
Vinicius Martins Cavalcanti <88601887+Vinizik@users.noreply.github.com>
2023-06-14 18:30:18 -0300

    Utilizando a Annotation @Autowired e as funções save(), findAll(), findBy() e count()

--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -1,12 +1,31 @@
 package br.com.projeto.api.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
     
-    //Atributo
+    // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
     private String nome;
-    private int idade;
+    private int idade;  
 
-    //Get e Set
+    // Gets e Sets
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
     public String getNome() {
         return nome;
     }
---
+++ src/main/java/br/com/projeto/api/repositorio/Repositorio.java
@@ -0,0 +1,18 @@
+package br.com.projeto.api.repositorio;
+
+import java.util.List;
+// Importa o CrudRepository e a annotation @Repository
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+// Importa o modelo Pessoa
+import br.com.projeto.api.modelo.Pessoa;
+
+@Repository
+public interface Repositorio extends CrudRepository<Pessoa, Integer> {
+
+    List<Pessoa> findAll();
+
+    Pessoa findByCodigo(int codigo);
+    
+}
